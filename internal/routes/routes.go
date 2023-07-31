b0a2cc0d392cf22f19a084346aace0a387b4ec6e
xiwangly2 <1334850101@qq.com>
2023-07-31 20:00:06 +0800

    Add Error suppression

--- internal/routes/routes.go
+++ internal/routes/routes.go
@@ -2,19 +2,30 @@ package routes
 
 import (
 	"net/http"
-
 	"xwgo/internal/handlers"
 )
+
+// Wrapper function to handle panics and return Internal Server Error
+func panicHandler(handler http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		defer func() {
+			if err := recover(); err != nil {
+				http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+			}
+		}()
+		handler(w, r)
+	}
+}
 
 func NewRouter() *http.ServeMux {
 	mux := http.NewServeMux()
 
-	// Register routes
-	mux.HandleFunc("/", handlers.RootHandler) // Handle root URL
-	mux.HandleFunc("/ua", handlers.UAHandler)
-	mux.HandleFunc("/qq", handlers.QQHandler)
-	mux.HandleFunc("/yiyan", handlers.YiYanDataHandler)
-	mux.HandleFunc("/ping", handlers.PingHandler)
+	// Register routes with the panicHandler
+	mux.HandleFunc("/", panicHandler(handlers.RootHandler))
+	mux.HandleFunc("/ua", panicHandler(handlers.UAHandler))
+	mux.HandleFunc("/qq", panicHandler(handlers.QQHandler))
+	mux.HandleFunc("/yiyan", panicHandler(handlers.YiYanDataHandler))
+	mux.HandleFunc("/ping", panicHandler(handlers.PingHandler))
 
 	return mux
 }
